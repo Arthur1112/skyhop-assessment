765c7e2abba0b418bbd9490014bd87593a1f6731
Arthur <arthur_2335@hotmail.com>
2024-02-27 21:51:14 -0500

    created button comp

---
+++ src/components/button.tsx
@@ -0,0 +1,28 @@
+import '../styles/button.css';
+export interface ButtonProps {
+  onClick?: () => {};
+  text?: string;
+  variant?: 'primary' | 'secondary' | 'close' | 'submit';
+}
+
+const Button = ({ onClick, text, variant = 'secondary' }: ButtonProps) => {
+  const variantStyle = () => {
+    switch (variant) {
+      case 'secondary':
+        return 'secondaryContainer';
+      case 'close':
+        return 'primaryContainer closeContainer';
+      case 'submit':
+        return 'primaryContainer submitContainer';
+      default:
+        return 'primaryContainer';
+    }
+  };
+  return (
+    <button onClick={onClick} className={variantStyle()}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
